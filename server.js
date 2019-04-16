3f4a0b9df857fa106f269b3dbec6db1cc1367fc4
Gal Ofir <gal.ofir.go@gmail.com>
2019-04-16 20:45:24 +0300

    asdsa

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('express')();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Its a fucking start'));
 
 app.listen(port, () =>
     console.log(`Server up on port ${port}`));
